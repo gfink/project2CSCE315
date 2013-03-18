78355d10fbe18cfd0a76a7d850ad5284e1fb000f
Eric Garfinkle <ericgarfinkle@gmail.com>
2013-03-17 22:17:30 -0500

    changes by travis, uploaded by me

--- Project 2/Fanorona/src/eighteen/Points.java
+++ Project 2/Fanorona/src/eighteen/Points.java
@@ -2,8 +2,8 @@ package eighteen;
 import java.util.ArrayList;
 
 public class Points {
-	private int row;
-	private int column;
+	public int row;
+	public int column;
 	ArrayList<Points> adjacentLocations;
 	
 	public Points(int x, int y) {
@@ -21,7 +21,7 @@ public class Points {
 		return false;
 	}
 	
-	public boolean isValidSpace(int x, int y) {
+	public static boolean isValidSpace(int x, int y) {
 		if(x < BoardManager.ROWS && x >= 0 && y < BoardManager.COLUMNS && y >= 0)
 			return true;
 		return false;
