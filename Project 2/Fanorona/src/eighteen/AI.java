ca37ff44a9920ccf9363acc6c366319d9e2fb340
Joseph Newman <joseph.newman@live.com>
2013-03-20 17:55:18 -0500

    Started working on AI and Tree

---
+++ Project 2/Fanorona/src/eighteen/AI.java
@@ -0,0 +1,16 @@
+package eighteen;
+
+public class AI {
+	// Will need to implement Thread at some point in the future
+	
+	private Tree minMaxTree;
+	private Pieces myColor;
+	
+	public AI(Pieces color) {
+		minMaxTree = new Tree();
+		myColor = color;
+	}
+	
+	// Need to define addLevel
+	// Pruning
+}
